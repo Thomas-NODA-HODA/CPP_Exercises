ff5236632e1ce314b1b19b8aed1833fb88bf1265
Thomas NODA HODA <thomas.noda205@gmail.com>
2024-01-19 12:21:26 +0100

    tp1 - ex2 - q6 - not done

--- tp1/ex2/main.cpp
+++ tp1/ex2/main.cpp
@@ -1,13 +1,39 @@
 #include <iostream>
+#include <vector>
 
 #include "Person.hpp"
 
 int main() {
-    Person person = Person("Palluche", "La Faluche");
-    //person.first_name = "Palluche";
-    //person.surname = "La Faluche";
+    std::cout << "Nombre de personnes: ";
+    int number;
+    std::cin >> number;
 
-    std::cout << "La personne s'appelle " << person.get_first_name() << " " << person.get_surname() << std::endl;
+    std::vector<Person> persons(number);
+
+    std::string first_name;
+    std::string surname;
+
+    for(int i = 0; i < number; ++i) {
+        std::cout << "Prenom: ";
+        std::cin >> first_name;
+        std::cout << "Nom: ";
+        std::cin >> surname;
+
+        Person person = Person{first_name, surname};
+
+        persons.push_back(person);
+    }
+    
+    std::cout << "Les personnes sont ";
+
+    std::string separator = "";
+
+    for(auto person : persons) {
+        std::cout << separator << person.get_first_name() << " " << person.get_surname();
+        separator = ", ";
+    }
+
+    std::cout << std::endl;
 
     return 0;
 }
